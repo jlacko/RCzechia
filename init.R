c8f21759803f004254d0f8238f26eb3e741a5830
jlacko <jindra.lacko@gmx.net>
2017-07-20 19:39:28 +0200

    initial commit

---
+++ init.R
@@ -0,0 +1,15 @@
+library(devtools)
+
+#read data...
+republika <- readRDS("~/GeoCzech/Republika.rds")
+okresy <- readRDS("~/GeoCzech/Okresy.rds")
+obce <- readRDS("~/GeoCzech/Obce.rds")
+reky <- readRDS("~/GeoCzech/Reky.rds")
+plochy <- readRDS("~/GeoCzech/Plochy.rds")
+
+#save data...
+devtools::use_data(republika)
+devtools::use_data(okresy)
+devtools::use_data(obce)
+devtools::use_data(reky)
+devtools::use_data(plochy)

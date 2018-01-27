63ded01a985feae9a28357d1dd04413420694983
Jindra Lacko <jindra.lacko@gmail.com>
2018-01-27 23:54:22 +0100

    sf first service

--- R/republika.R
+++ R/republika.R
@@ -1,8 +1,6 @@
 #' Boundaries of the Czech Republic.
 #'
-#' A Large SpatialPolygonsDataFrame containing the Czech Republic.
-#'
-#' @format A Large SpatialPolygonsDataFrame containing the Czech Republic.
+#' @format Boundaries of the Czech Republic, as a sf data frame:
 #'
 #' @source \url{https://www.arcdata.cz/produkty/geograficka-data/arccr-500}
 "republika"

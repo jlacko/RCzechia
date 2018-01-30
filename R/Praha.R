b7abe610581dee9de1aa452e18e9f208fcf1073e
Jindra <jindra.lacko@gmail.com>
2018-01-30 10:00:28 +0100

    updated casti, resolution = 5m

--- R/Praha.R
+++
@@ -1,9 +0,0 @@
-#' Districts (mestske casti) of Prague.
-#'
-#' @format 57 districts (mestske casti) of Prague, as a sf data frame:
-#' \describe{
-#'   \item{KOD_MOaMC}{Code of the city part / kod mestske casti}
-#'   \item{NAZ_ZKR_MOaMC}{Short name of the city part / zkraceny nazev mestske casti}
-#' }
-#' @source \url{https://www.arcdata.cz/produkty/geograficka-data/arccr-500}
-"Praha"

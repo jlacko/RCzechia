47d99a535ce0effe700fb2e02a15936cb65c3d27
jlacko <jindra.lacko@gmail.com>
2018-02-21 07:42:42 +0000

    manuals triple checked, copyright updated in description

--- R/okresy.R
+++ R/okresy.R
@@ -4,10 +4,11 @@
 #' @format 77 districts of the Czech Republic, as a sf data frame:
 #' \describe{
 #'   \item{KOD_OKRES}{Code of the district (okres).}
-#'   \item{KOD_LAU1}{Code of the district (okres), primary key. Use this as key to add other data items.}
-#'   \item{NAZ_LAU1}{Name of the district (okres).}
+#'   \item{KOD_LAU1}{Code of the district as LAU1 unit (okres), primary key. Use this as key to add other data items.}
+#'   \item{NAZ_LAU1}{Name of the district as LAU1 unit (okres).}
 #'   \item{KOD_KRAJ}{Code of the region.}
 #'   \item{KOD_CZNUTS3}{Code of the region as NUTS3 (kraj).}
 #'   \item{NAZ_CZNUTS3}{Name of the region (kraj).}
+#' }
 #' @source \url{https://www.arcdata.cz/produkty/geograficka-data/arccr-500}
 "okresy"

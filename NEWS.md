5351026cf1475c169e3074152b61c9b50c1764ad
Jindra Lacko <jindra.lacko@gmail.com>
2018-02-21 20:56:34 +0100

    Merge cran-dev to master (#5)
    * cran version - working draft
    
    * headings enlarged to improve orientation
    
    * level 3 instead of lvl. 2 heading
    
    * heading working...
    
    * Update README.md
    
    * cleaning up readme
    
    * manuals double checked to conform to sf format
    
    * manuals triple checked, copyright updated in description
    
    * links in description
    
    * arcdata communication
    
    * no longer depending on sf - one less note on CRAN
    
    * cleaner description, correct translation of ORP in man, removed savviness map
    
    * cran comments created

---
+++ NEWS.md
@@ -0,0 +1,4 @@
+version 1.0.3
+  - this is the the first CRAN version
+  - a development version is available on https://github.com/jlacko/RCzechia
+  - if you have any questions or suggestions, please contact me (jindra dot lacko at gmail dot com)
--- R/orp_polygony.R
+++ R/orp_polygony.R
@@ -1,14 +1,14 @@
-#' Level III Municipalities / communes (obce s rozsirenou pusobnosti) of the Czech Republic
+#' Municipalities with extended powers (obce s rozsirenou pusobnosti) of the Czech Republic
 #'
-#' @format 205 level III municipalities of the Czech Republic , as a sf data frame:
+#' @format 205 municipalities with extended powers of the Czech Republic , as a sf data frame:
 #' \describe{
 #'   \item{KOD_ORP}{Code of the level III commune (obec s rozsirenou pusobnosti).}
 #'   \item{NAZ_ZKR_ORP}{Short name of the level III commune (obec s rozsirenou pusobnosti).}
 #'   \item{NAZ_ORP}{Full name of the level III commune (obec s rozsirenou pusobnosti).}
 #'   \item{KOD_RUIAN}{RUIAN (Registr uzemní identifikace, adres a nemovitosti) code.}
 #'   \item{KOD_OKRES}{Code of the district (okres).}
-#'   \item{KOD_LAU1}{Code of the level 1 administrative unit (okres).}
-#'   \item{NAZ_LAU1}{Name of the level 1 administrative unit (okres).}
+#'   \item{KOD_LAU1}{Code of the LAU1 administrative unit (okres).}
+#'   \item{NAZ_LAU1}{Name of the LAU1 administrative unit (okres).}
 #'   \item{KOD_KRAJ}{Code of the region (kraj).}
 #'   \item{KOD_KRAJ}{Code of the region (kraj).}
 #'   \item{KOD_CZNUTS2}{Code of the NUTS3 unit (kraj)}

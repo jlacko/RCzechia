e585194c07b0e338373b97bdac6e361a06ee025b
jlacko <jindra.lacko@gmail.com>
2018-03-28 13:59:36 +0000

    version upgrade 1.1 > 1.2, news + readme updated, tests rearranged

--- README.md
+++ README.md
@@ -6,7 +6,9 @@ The shapefiles are based on ArcČR 500 (https://www.arcdata.cz/produkty/geografi
  * the encoding was adjusted to handle Czech accents correctly in R  
  * coordinate reference system was changed from a local CRS (S-JSTK) to global WGS84 ([EPSG:4326](https://epsg.io/4326))   
  * demographic data were removed, as they get out of date rather fast and are very easy to re-attach using the `sf` package workflow  
- * the shapefiles were slightly simplified to fit into memory better
+ * the shapefiles were slightly simplified to fit into memory better  
+
+The default resolution is 1 meter; for the most commonly used shapes(*republika*, *kraje* and *okresy*) a low resolution shapefile is included. This lo-res shapefile does not require a working internet connection. To use the high resolution (default) shapefile a working intenet connection is required, as the data files were too big to meet CRAN requirements on package size and must be stored externally.
 
 ### A note to Czech users
 Tohle je "oficiální", a tedy anglické, readme. Českou verzi naleznete na http://www.jla-data.net/cze/package-rczechia/
@@ -22,7 +24,7 @@ You can also get the latest development version by running `devtools::install_gi
 * **republika**: borders of the Czech Republic
 * **kraje**: 14 regions of the Czech Republic + Prague.  
 Key is KOD_CZNUTS3 (CZ NUTS3 code).
-* **okresy**: 76 districts (LAU1 areas) of the Czech Republic + Prague (legally not *a district* but *the capital*). 
+* **okresy**: 76 districts (LAU1 areas) of the Czech Republic + Prague (legally not *a district* but *the capital*).  
 Key is KOD_LAU1 (CZ LAU1 code).
 * **orp_polygony** 205 municipalities with extended powers (in Czech: obce s rozšířenou působností) + Prague (legally not *a city* but *the capital*).  
 Key is KOD_ORP.
@@ -35,4 +37,4 @@ Key is KOD.
 * **reky**: streams and rivers
 * **plochy**: stillwaters (lakes and ponds).
 
-All objects are functions returning data frames, so must be followed by empty brackets (i.e. `hranice <- republika()`).
+All objects are implemented as functions returning data frames, so must be followed by brackets (i.e. `hranice <- republika()`).
--- tests/testthat/test-jla.R
+++ tests/testthat/test-jla.R
@@ -1,13 +1,30 @@
 library(testthat)
 library(httr)
 
-context("městské části")
-  expect_that(is.data.frame(casti()), is_true())
-  expect_equal(nrow(casti()), 142)
+context("republika")
+  expect_that(is.data.frame(republika()), is_true())
+  expect_equal(nrow(republika()), 1)
 
 context("kraje")
   expect_that(is.data.frame(kraje()), is_true())
   expect_equal(nrow(kraje()), 14)
+
+
+context("okresy")
+  expect_that(is.data.frame(okresy()), is_true())
+  expect_that(is.data.frame(okresy("low")), is_true())
+  expect_that(is.data.frame(okresy("high")), is_true())
+
+  expect_equal(nrow(okresy()), 77)
+  expect_equal(nrow(okresy("low")), 77)
+  expect_equal(nrow(okresy("high")), 77)
+
+  expect_error(okresy("bflm")) # neznámé rozlišení - očekávám high(default) / low
+  expect_that(object.size(okresy("low")) < object.size(okresy("high")), is_true()) # low res je menší než high res
+
+context("ORP")
+  expect_that(is.data.frame(orp_polygony()), is_true())
+  expect_equal(nrow(orp_polygony()), 206)
 
 context("obce body")
   expect_that(is.data.frame(obce_body()), is_true())
@@ -17,19 +34,9 @@ context("obce polygony")
   expect_that(is.data.frame(obce_polygony()), is_true())
   expect_equal(nrow(obce_polygony()), 6258)
 
-context("okresy")
-  expect_that(is.data.frame(okresy()), is_true())
-  expect_that(is.data.frame(okresy("low")), is_true())
-  expect_that(is.data.frame(okresy("high")), is_true())
-  expect_error(okresy("bflm")) # neznámé rozlišení - očekávám high(default) / low
-  expect_equal(nrow(okresy()), 77)
-  expect_equal(nrow(okresy("high")), 77)
-  expect_equal(nrow(okresy("low")), 77)
-  expect_that(object.size(okresy("low")) < object.size(okresy("high")), is_true()) # low res je menší než high res
-
-context("ORP")
-  expect_that(is.data.frame(orp_polygony()), is_true())
-  expect_equal(nrow(orp_polygony()), 206)
+context("městské části")
+  expect_that(is.data.frame(casti()), is_true())
+  expect_equal(nrow(casti()), 142)
 
 context("vodní plochy")
   expect_that(is.data.frame(plochy()), is_true())
@@ -39,6 +46,3 @@ context("řeky")
   expect_that(is.data.frame(reky()), is_true())
   expect_equal(nrow(reky()), 6198)
 
-context("republika")
-  expect_that(is.data.frame(republika()), is_true())
-  expect_equal(nrow(republika()), 1)

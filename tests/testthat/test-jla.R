766f8958bc923ed4a0707701f767e413e03b1bfa
jlacko <jindra.lacko@gmail.com>
2018-05-07 17:15:17 +0000

    unionSF tests with tolerance 1e-6

--- tests/testthat/test-jla.R
+++ tests/testthat/test-jla.R
@@ -119,13 +119,16 @@ context("unionSF")
     unionSF('NAZ_OBEC') %>%
     filter(key == 'Praha')
 
+  ofiko_praha <- kraje() %>% # Praha jako kraj
+    filter(KOD_CZNUTS3 == "CZ010")
+
   expect_equal(st_contains(republika("high"), united_praha)[[1]], 1) # Praha z částí je v republice
   expect_equal(st_contains(united_praha, obec_praha)[[1]], 1)  # bod Praha je ve spojené Praze
 
   expect_equal(st_crs(casti()), st_crs(united_praha)) # CRS na vstupu = CRS na výstupu
 
-  expect_equal(st_area(united_praha), st_area(okres_praha), tolerance = 5)
-      # Praha z částí a Praha jako low res okres jsou stejně velké, plus mínus pět m^2
+  expect_equal(st_area(united_praha), st_area(ofiko_praha), tolerance = 1e-6)
+      # Praha z částí a Praha jako kraj jsou stejně velké, plus mínus jedna miliontina
 
 
 

020e425e3ad124001d006874c52dd13bf5ca8e2b
Jindra Lacko <jindra.lacko@gmail.com>
2020-06-04 16:17:18 +0200

    updated travis package list

--- pre-submission.R
+++ pre-submission.R
@@ -1,7 +1,9 @@
 # pre-submission routine
 
+rhub::platforms() # to find an appropriate macos version
+
 rhub::check_on_windows()
-rhub::check_on_macos()
+rhub::check_for_cran(platforms = "macos-highsierra-release")
 devtools::check_win_release()
 devtools::check_win_devel()
 

24fa0108357795d6ba6761973ee437cd2660295e
Jindra Lacko <jindra.lacko@gmail.com>
2020-09-10 10:47:52 +0200

    + unit tests for admin areas / known locations

--- tests/testthat/test-1-data-structures.R
+++ tests/testthat/test-1-data-structures.R
@@ -446,4 +446,20 @@ expect_true(st_contains(okres_praha, ctverec_praha, sparse = F)[[1]]) # čtverec
 
 expect_false(st_contains(low_res_stc, obec_praha, sparse = F)[[1]]) # bod Praha není ve Středních Čechách (je v Praze)
 
+# Kramářova vila je v Praze / obci, orp, okresu i kraji
+
+vila <- geocode("Gogolova 212/1, Praha 1")
+
+expect_equal(st_join(vila, kraje(), left = F)$KOD_CZNUTS3, "CZ010") # Kramářova vila je v kraji Praha
+expect_equal(st_join(vila, okresy(), left = F)$KOD_LAU1, "CZ0100") # Kramářova vila je v okrese Praha
+expect_equal(st_join(vila, orp_polygony(), left = F)$KOD_ORP, "1000") # Kramářova vila je v ORP Praha
+expect_equal(st_join(vila, obce_polygony(), left = F)$KOD_OBEC, "554782") # Kramářova vila je v obci Praha
+
+# Stezka v oblacích je na Dolní Moravě
+stezka <- geocode("Velká Morava 46, Dolní Morava")
+
+expect_equal(st_join(stezka, kraje(), left = F)$KOD_CZNUTS3, "CZ053") # Stezka v oblacích je v Pardubickém kraji
+expect_equal(st_join(stezka, okresy(), left = F)$KOD_LAU1, "CZ0534") # Stezka v oblacích je v okrese Ústí nad Orlicí
+expect_equal(st_join(stezka, orp_polygony(), left = F)$KOD_ORP, "5305") # Stezka v oblacích je v ORP Králíky
+expect_equal(st_join(stezka, obce_polygony(), left = F)$KOD_OBEC, "580163") # Stezka v oblacích je v obci Dolní Morava
 

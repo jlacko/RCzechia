b46e337db4d2f27aee7cc01c138cff96d1c0dee7
Jindra Lacko <jindra.lacko@gmail.com>
2020-10-23 22:59:57 +0200

    solaris patched = a known troublemaker

--- pre-submission.R
+++ pre-submission.R
@@ -4,6 +4,7 @@ rhub::platforms() # to find an appropriate macos version
 
 rhub::check_on_windows()
 rhub::check_for_cran(platforms = "macos-highsierra-release-cran")
+rhub::check_for_cran(platforms = "solaris-x86-patched")
 devtools::check_win_release()
 devtools::check_win_devel()
 devtools::check_win_oldrelease()

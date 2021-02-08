e35540a17973dbec69236fd673efcb7e0cebcf79
Jindra Lacko <jindra.lacko@gmail.com>
2021-02-08 22:21:19 +0100

    digest senát gpkg

--- data-raw/digest-senat.R
+++ data-raw/digest-senat.R
@@ -9,7 +9,14 @@ library(dplyr)
 
 rozhodne_datum <- "2021-02"
 
-okrsky_senat <- st_read("./data-raw/vo_senat_010616.shp") %>%
-  mutate(NAZEV_VO = iconv(NAZEV_VO, from = "UTF-8", to = "UTF-8"))
+senat_high_res <- st_read("./data-raw/senat.gpkg")
 
+senat_low_res <- senat_high_res %>%
+  rmapshaper::ms_simplify(keep = .005,
+                          keep_shapes = T)
+
+plot(st_geometry(senat_low_res))
+
+saveRDS(senat_high_res, paste0("./data-backup/Senat-high-", rozhodne_datum, ".rds"))
+saveRDS(senat_low_res, paste0("./data-backup/Senat-low-", rozhodne_datum, ".rds"))
 

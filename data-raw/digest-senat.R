9fddbad053b7535fd3f006816f0c0734351c3f34
Jindra Lacko <jindra.lacko@gmail.com>
2021-02-11 22:45:26 +0100

    major indigestion of senát

--- data-raw/digest-senat.R
+++ data-raw/digest-senat.R
@@ -10,19 +10,16 @@ library(dplyr)
 rozhodne_datum <- "2021-02"
 
 senat_high_res <- st_read("./data-raw/senat.gpkg", stringsAsFactors = F) %>%
+  st_set_crs("EPSG:4326") %>%
   st_make_valid() %>%
   st_cast()
 
 colnames(senat_high_res) <- c("OBVOD", "SIDLO", "NAZEV_VO", "geometry")
 st_geometry(senat_high_res) <- "geometry"
 
-senat_high_res <- senat_high_res %>%
-  st_transform("EPSG:4326")
-
 senat_low_res <- senat_high_res %>%
   rmapshaper::ms_simplify(keep = .005,
                           keep_shapes = T)
-
 
 saveRDS(senat_high_res, paste0("./data-backup/Senat-high-", rozhodne_datum, ".rds"))
 saveRDS(senat_low_res, paste0("./data-backup/Senat-low-", rozhodne_datum, ".rds"))

10581ef5d8c744008b349693456c531a0b9ff085
Jindra Lacko <jindra.lacko@gmail.com>
2021-07-09 15:55:56 +0200

    docker in principle working

---
+++ data-raw/run_ozymandias.sh
@@ -0,0 +1,8 @@
+docker build -t ozymandias .
+docker run \
+ -e  USER=jindra -e PASSWORD=Montana \
+ -v /home/jindra/Documents/RCzechia/data-raw:/home/jindra/data-raw \
+ -v /home/jindra/Documents/RCzechia/data-backup:/home/jindra/data-backup \
+ --rm \
+ -p 8787:8787 \
+ ozymandias

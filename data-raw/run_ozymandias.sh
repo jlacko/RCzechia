140af48d67a4a1ed5b31ace1b9a02b3833e6a840
Jindra Lacko <jindra.lacko@gmail.com>
2022-04-01 16:07:57 +0200

    clean up detritus after docker run

--- data-raw/run_ozymandias.sh
+++ data-raw/run_ozymandias.sh
@@ -1,6 +1,11 @@
+# run the docker
 docker run \
  -e DISABLE_AUTH=true \
  -v $(pwd)/../:/home/rstudio/ \
  --rm \
  -p 8787:8787 \
  ozymandias
+
+# clean up detritus...
+rm -rf ../.config
+rm -rf ../.local

12855ca37208991779fc60a8c4939d6f1461cf53
Jindra Lacko <jindra.lacko@gmail.com>
2022-11-23 18:34:09 +0100

    close terrain dem (#49)
    * raster digest working in principle
    
    * reliéfy se vcelku líbí
    
    * tests complete locally
    
    * vignette updated
    
    * tests rewrite
    
    * typo tuning

--- R/vyskopis.R
+++ R/vyskopis.R
@@ -1,42 +1,65 @@
 #' Vyskopis
 #'
-#' Terrain of the Czech Republic as a {raster} package object.
+#' Terrain of the Czech Republic as a {terra} package object.
 #'
-#' The function returns a raster file of either actual relief (values are meters above sea level) or rayshaded relief (created via highly recommended {rayshader} package).
+#' The function returns a raster file of either actual terrain (values are meters above sea level) or rayshaded relief..
 #'
-#' The raster is 5084 by 3403 cells, meaning each pixel is about 90 × 90 meters. It works the best at level of country or regions, at the level of a city or lower it may be somewhat grainy.
+#' The raster is created from EU DEM 1.1 file by Copernicus Land Monitoring service. The original file has pixel resolution 25×25 meters, which is too detailed for purposes of the package and was downsampled by factor of 4.
+#'
+#' The extent of the raster file is bouding box of the Czech Republic; this is a change to prior versions in order to better facilitate use of the raster in natural sciences context. To preserve compatibility optional argument `cropped` has been created, defaulting to `TRUE` (i.e. behaviour before v1.10.0).
 #'
 #' Due to package size constraints both versions are stored externally (and a working internet connection is required to use the package).
 #'
-#' The data is current to September 2016. Downloaded size of the rayshaded raster is 8.4 MB, actual raster is 31.4 MB.
+#' The data is current to year 2011 (but it is not expected to materially change over time). Downloaded size of the rasters is 52 MB.
 #'
-#' @param format Should the function return actual relief (meters above sea level) or shaded relief (rayshaded). Allowed values are "actual" and "rayshaded".
+#' @param format Should the function return actual terrain (meters above sea level) or shaded relief (rayshaded). Allowed values are "actual" and "rayshaded".
 #'
-#' @return \code{raster} package RasterLayer.
+#' @param cropped Should the raster provide data over Czech Republic's bounding box (cropped = FALSE) or just actual borders (cropped = TRUE). Defaults to TRUE to preserve compatiblity with earlier versions.
 #'
-#' @source © ArcČR, ARCDATA PRAHA, ZÚ, ČSÚ, 2016 \url{https://www.arcdata.cz/produkty/geograficka-data/arccr-4}
+#' @return \code{terra} package SpatRaster
+#'
+#' @source Copernicus Land Monitoring service, with funding by the European Union. \url{https://land.copernicus.eu/imagery-in-situ/eu-dem}
 #'
 #' @examples
 #' \donttest{
-#' library(raster)
+#' library(terra)
 #'
-#' relief <- vyskopis("rayshaded")
+#' # original extent - bounding box over Czech Republic
+#' original_extent <- vyskopis("rayshaded", cropped = FALSE)
 #'
-#' plot(relief, col = gray.colors(16))
+#' plot(original_extent, col = gray.colors(16))
+#'
+#' # add plot of country borders, for context
+#' plot(RCzechia::republika(),
+#'      border = "red",
+#'      col = NA,
+#'      add = TRUE)
+#'
+#' # cropped to size - default behaviour
+#' cropped_extent <- vyskopis("rayshaded")
+#'
+#' plot(cropped_extent, col = gray.colors(16))
+#'
 #' }
 #' @export
 
 
-vyskopis <- function(format = "rayshaded") {
+vyskopis <- function(format = "rayshaded", cropped = TRUE) {
   if (!is.element(format, c("actual", "rayshaded"))) {
     stop(paste(format, "is not a valid format; recognized values are \"actual\" or \"rayshaded\"."))
-  } # /if - valid resolution
+  } # /if - valid format
+  if (!is.element(cropped, c(TRUE, FALSE))) {
+    stop(paste(cropped, "is not a valid value for cropped; recognized values are \"TRUE\" or \"FALSE\"."))
+  } # /if - valid valid cropped
 
   if (format == "rayshaded") {
-    result <- .downloader("Vyskopis-stiny.rds")
+    result <- .downloader("vyskopis-shaded-dem.tif")
   } else {
-    result <- .downloader("Vyskopis-vyska.rds")
+    result <- .downloader("vyskopis-dem.tif")
   } # /if - download of result
+
+  # need to handle CRAN policy
+  if(cropped & !is.null(result)) result <- terra::crop(result, terra::vect(RCzechia::republika("low")), mask = TRUE)
 
   result
 } # /function
--- tests/testthat/test-1-relief.R
+++ tests/testthat/test-1-relief.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(httr)
+library(terra)
 
 test_that("reliéf", {
 
@@ -9,25 +10,33 @@ test_that("reliéf", {
   expect_message(vyskopis(), "internet") # zpráva o chybějícím internetu
   Sys.setenv("NETWORK_UP" = TRUE)
 
-  expect_s4_class(vyskopis(), "RasterLayer")
-  expect_s4_class(vyskopis("actual"), "RasterLayer")
-  expect_s4_class(vyskopis("rayshaded"), "RasterLayer")
+  expect_s4_class(vyskopis(), "SpatRaster")
+  expect_s4_class(vyskopis("actual"), "SpatRaster")
+  expect_s4_class(vyskopis("rayshaded"), "SpatRaster")
+  expect_s4_class(vyskopis("actual", FALSE), "SpatRaster")
+  expect_s4_class(vyskopis("rayshaded", FALSE), "SpatRaster")
 
-  # test velikosti
-  expect_equal(vyskopis()@ncols, 5084) # sloupce jsou všechny
-  expect_equal(vyskopis("actual")@ncols, 5084) # sloupce jsou všechny
-  expect_equal(vyskopis("rayshaded")@ncols, 5084) # sloupce jsou všechny
+  # test rozsahu
+  expect_equal(terra::expanse(vyskopis()), units::drop_units(st_area(republika("low"))), tolerance = 1/100)
+  expect_equal(terra::expanse(vyskopis("actual")), units::drop_units(st_area(republika("low"))), tolerance = 1/100)
+  expect_equal(terra::expanse(vyskopis("rayshaded")), units::drop_units(st_area(republika("low"))), tolerance = 1/100)
 
-  expect_equal(vyskopis()@nrows, 3403) # řádky jsou všechny
-  expect_equal(vyskopis("actual")@nrows, 3403) # řádky jsou všechny
-  expect_equal(vyskopis("rayshaded")@nrows, 3403) # řádky jsou všechny
+  # oříznutý raster je menší než surový
+  expect_gt(terra::expanse(vyskopis(cropped = F)), terra::expanse(vyskopis(cropped = T)))
+  expect_gt(terra::expanse(vyskopis("rayshaded", cropped = F)), terra::expanse(vyskopis("rayshaded", cropped = T)))
+  expect_gt(terra::expanse(vyskopis("actual", cropped = F)), terra::expanse(vyskopis("actual", cropped = T)))
 
   # test projekce - WGS84 pure & unadultered
-  expect_equal(projection(crs(vyskopis())), "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0")
-  expect_equal(projection(crs(vyskopis("actual"))), "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0")
-  expect_equal(projection(crs(vyskopis("rayshaded"))), "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0")
+  expect_equal(st_crs(vyskopis())$input, "WGS 84")
+  expect_equal(st_crs(vyskopis("actual"))$input, "WGS 84")
+  expect_equal(st_crs(vyskopis("rayshaded"))$input, "WGS 84")
+  expect_equal(st_crs(vyskopis("actual", FALSE))$input, "WGS 84")
+  expect_equal(st_crs(vyskopis("rayshaded", FALSE))$input, "WGS 84")
 
   # očekávaná chyba
   expect_error(vyskopis("bflm")) # neznámé rozlišení - očekávám actual / rayshaded
+  expect_error(vyskopis("actual", cropped = "bflm")) # cropped usí být boolean
+  expect_error(vyskopis("actual", cropped = NA))
+
 })
 

f405a35ff3e7fc4cf2c4cc1eeb66f63ee8878a69
Jindra Lacko <jindra@jla-data.net>
2025-06-15 00:03:30 +0200

    území update

--- data-raw/digest-chr-uzemi-aopk.R
+++ data-raw/digest-chr-uzemi-aopk.R
@@ -3,13 +3,13 @@ library(sf)
 library(dplyr)
 
 # data source = https://data.nature.cz- neboli AOPK
-velkoplosna <- st_read("./data-raw/Velkoplo%C5%A1n%C3%A1_zvl%C3%A1%C5%A1t%C4%9B_chr%C3%A1n%C4%9Bn%C3%A1_%C3%BAzem%C3%AD.shp") %>%
+velkoplosna <- st_read("./data-raw/Velkoplošná_zvláště_chráněná_území.shp") %>%
   select(TYP = KAT, NAZEV) %>%
   mutate(PLOCHA = "velkoplošná") %>%
   st_make_valid() %>%
   st_transform(4326) # pro jistotu...
 
-maloplosna <- st_read("./data-raw/Maloplo%C5%A1n%C3%A1_zvl%C3%A1%C5%A1t%C4%9B_chr%C3%A1n%C4%9Bn%C3%A1_%C3%BAzem%C3%AD.shp") %>%
+maloplosna <- st_read("./data-raw/Maloplošná_zvláště_chráněná_území.shp") %>%
   select(TYP = KAT, NAZEV) %>%
   mutate(PLOCHA = "maloplošná") %>%
   st_make_valid() %>%
@@ -19,4 +19,4 @@ chr_uzemi <- rbind(velkoplosna, maloplosna)
 
 st_crs(chr_uzemi) <- "EPSG:4326" # sladění na testy; na funkci vliv nemá...
 
-saveRDS(chr_uzemi, "./data-backup/ChrUzemiAOPK-2021-02.rds")
+saveRDS(chr_uzemi, "./data-backup/ChrUzemiAOPK-2025-06.rds")
